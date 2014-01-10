a1d7d19c979ed538ca48c05e845e60bdd2386eeb
Richard Healy <richard.healy@basekit.com>
2014-01-10 11:21:52 +0000

    Removed templates from templates.js

--- js/templates.js
+++ js/templates.js
@@ -5,33 +5,5 @@ var templates = [
     {
         name: "Pearl",
         directory: "pearl"
-    },
-    {
-        name: "BaseKit",
-        directory: "basekit-com-website"
-    },
-    {
-        name: "sitejam",
-        directory: "sitejam"
-    },
-    {
-        name: "tarifa",
-        directory: "tarifa"
-    },
-    {
-        name: "newave",
-        directory: "newave"
-    },
-    {
-        name: "trendsetter",
-        directory: "trendsetter"
-    },
-    {
-        name: "grillhouse",
-        directory: "grillhouse"
-    },
-    {
-        name: "muse",
-        directory: "muse"
     }
 ];
